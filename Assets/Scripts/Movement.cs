5f7cc3ff370bb4fc8d15d49546adfd8fa7a7e049
pedrohfcarneiro <phtriplox8090@gmail.com>
2022-06-06 18:20:51 -0300

    add project to github

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Vector2 input = new Vector2();
+    public float speed = 2f;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        input = new Vector2(x, y);
+
+        Vector2 movement = Vector2.ClampMagnitude(input, 2f);
+        Debug.Log(movement);
+
+        transform.position += new Vector3(movement.x, movement.y, 0) * speed * Time.deltaTime;
+    }
+}
